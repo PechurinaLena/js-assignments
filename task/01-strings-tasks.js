4603d9958a762937859ad55a94ef9ee25ea26314
PechurinaLena <lena8way@gmail.com>
2019-04-12 23:11:26 +0300

    getFirstChar test finish

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -88,7 +88,8 @@ function extractNameFromTemplate(value) {
  *   'cat'       => 'c'
  */
 function getFirstChar(value) {
-    throw new Error('Not implemented');
+    return value.substring(0, 1);
+    //throw new Error('Not implemented');
 }
 
 /**
