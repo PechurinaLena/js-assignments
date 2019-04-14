fe941fbe033c33a9c0da3a74804fe8e3ed1d24be
PechurinaLena <lena8way@gmail.com>
2019-04-14 17:08:28 +0300

    isTriangle test finish

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -90,6 +90,10 @@ function getSumBetweenNumbers(n1, n2) {
  *   10,10,10 =>  true
  */
 function isTriangle(a, b, c) {
+    let sides = [a, b, c];
+    let max = Math.max(...sides);
+    let sum = sides.reduce((a, b) => a + b, 0);
+    return sum - max > max;
     throw new Error('Not implemented');
 }
 
