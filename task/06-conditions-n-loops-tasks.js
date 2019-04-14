83ebd7bb17e4e25048167189ebbb1b5e7e5a3b43
PechurinaLena <lena8way@gmail.com>
2019-04-14 17:29:03 +0300

    reverseInteger test finish

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -250,6 +250,8 @@ function reverseString(str) {
  *   34143 => 34143
  */
 function reverseInteger(num) {
+    num = num + "";
+    return num.split("").reverse().join("");
     throw new Error('Not implemented');
 }
 
