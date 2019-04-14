c979550a39a696188c044971895b47155d00aa0d
PechurinaLena <lena8way@gmail.com>
2019-04-14 16:57:58 +0300

    getSumBetweenNumbers test finish

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -70,6 +70,7 @@ function getFactorial(n) {
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
 function getSumBetweenNumbers(n1, n2) {
+    return ((n2 - n1) + 1) * (n1 + n2) / 2;
     throw new Error('Not implemented');
 }
 
